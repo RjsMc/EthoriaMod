b76d19caacaf4ec661187032d26c8b1c521ba588
Jonathan <JonathanYin555@gmail.com>
2026-08-21 22:37:56 -0400

    Added Item

--- Content/TestItem.cs
+++ Content/TestItem.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
 namespace EthoriaMod.Content
 {
-    internal class TestItem
+    internal class TestItem : ModItem
     {
+        public override void SetDefaults()
+        {
+            
+        }
+
     }
 }
