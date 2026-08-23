a2dfa93477c47c22b14da9a26a9158527c225e57
Jonathan <JonathanYin555@gmail.com>
2026-08-22 20:02:44 -0400

    Troll

---
+++ Content/EthoriaPlayer/EthoriaPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EthoriaMod.Content.EthoriaPlayer
+{
+    public class EthoriaPlayer : ModPlayer
+    {
+        public float stamina = 0.0f;
+
+
+        public override void PreUpdateMovement()
+        {
+            Player.velocity = new Vector2(0, 0);
+        }
+    }
+}
