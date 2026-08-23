9b87c30500d7bf522537056ca0becb6e04400b85
Jonathan <JonathanYin555@gmail.com>
2026-08-22 22:03:41 -0400

    Sprint

--- Content/EthoriaPlayer/EthoriaPlayer.cs
+++ Content/EthoriaPlayer/EthoriaPlayer.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -13,11 +14,87 @@ namespace EthoriaMod.Content.EthoriaPlayer
     public class EthoriaPlayer : ModPlayer
     {
         public float stamina = 0.0f;
+        public bool sprinting = false;
 
+        private bool lastLeftControl = false;
+        private bool lastRightControl = false;
+        private bool controlLeftPressed = false;
+        private bool controlRightPressed = false;
+
+        private int lTap = 0;
+        private int rTap = 0;
+
+        private int doubleTapWindow = 15;
+
+        public override void PreUpdate()
+        {
+            if (!lastLeftControl) 
+            {
+                controlLeftPressed = Player.controlLeft;
+            }
+            if (!lastRightControl)
+            {
+                controlRightPressed = Player.controlRight;
+            }
+      
+            lastLeftControl = Player.controlLeft;
+            lastRightControl = Player.controlRight;
+        }
+
+        public override void PostUpdate()
+        {
+            controlLeftPressed = false;
+            controlRightPressed = false;
+            lTap--;
+            rTap--;
+
+        }
 
         public override void PreUpdateMovement()
         {
-            Player.velocity = new Vector2(0, 0);
+
+
+
+            float maxSpd = Player.maxRunSpeed;
+            float xSpd = float.Abs(Player.velocity.X);
+
+            if (rTap > 0 && rTap < doubleTapWindow && controlRightPressed)
+            {
+                if (float.Sign(Player.velocity.X) < 0 || xSpd < maxSpd)
+                {
+                    Player.velocity.X = maxSpd;
+                }
+                sprinting = true;
+            } else if (lTap > 0 && lTap < doubleTapWindow && controlLeftPressed)
+            {
+                if (float.Sign(Player.velocity.X) > 0 || xSpd < maxSpd)
+                {
+                    Player.velocity.X = -maxSpd;
+                }
+                sprinting = true;
+            }
+
+            if (!Player.controlLeft && !Player.controlRight)
+            {
+                sprinting = false;
+            }
+
+            if (controlLeftPressed)
+            {
+                lTap = doubleTapWindow;
+            }
+            if (controlRightPressed)
+            {
+                rTap = doubleTapWindow;
+            }
+        }
+        public override void PostUpdateRunSpeeds()
+        {
+            if (sprinting)
+            {
+                Player.maxRunSpeed *= 2;
+            }
+
         }
     }
 }
