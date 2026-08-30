db3054606b08455e2b113a0df43378f165e4f1db
Epiphonically <87289364+Epiphonically@users.noreply.github.com>
2026-08-30 01:18:42 -0400

    SkillTree Class

--- Content/SkillTree.cs
+++ Content/SkillTree.cs
@@ -10,48 +10,69 @@ namespace EthoriaMod.Content
 {
 
     
-    public class SkillTreeNode
+    
+    public class SkillTree
     {
-        public List<SkillTreeNode> Children;
-        public List<SkillTreeNode> Parents;
-        public int drawX, drawY;
-        public string skillName;
-        public bool unlocked;
-        public GrowDirection growDirection;
-        public enum GrowDirection
-        {
-            None,
-            Left,
-            Up,
-            Right,
-            Down,
-            enumSize
-        }
-        public SkillTreeNode(int drawX, int drawY, string skillName, GrowDirection growDirection = GrowDirection.None, bool unlocked = false)
-        {
-            this.drawX = drawX;
-            this.drawY = drawY;
-            this.skillName = skillName;
-            this.unlocked = unlocked;
-            this.growDirection = growDirection;
-        }
 
-        public SkillTreeNode addChild(string skillName)
-        {
-            SkillTreeNode child = new SkillTreeNode(0, 0, skillName, this.growDirection);
-            Children.Add(child);
-            child.Parents.Add(this);
+        public SkillTreeNode root;
+        public int dist;
 
-            return child;
-        }
-        public SkillTreeNode addChild(string skillName, GrowDirection direction)
+        public class SkillTreeNode
         {
-            SkillTreeNode child = new SkillTreeNode(this.drawX, this.drawY, skillName, direction);
-            Children.Add(child);
-            child.Parents.Add(this);
+            public List<SkillTreeNode>[GrowDirection.enumSize] Children;
+            public List<SkillTreeNode> Parents;
+            public int drawX, drawY;
+            public string skillName;
+            public bool unlocked;
+            public GrowDirection growDirection;
             
-            return child;
+            public enum GrowDirection
+            {
+                None,
+                Left,
+                Up,
+                Right,
+                Down,
+                enumSize
+            }
+            public SkillTreeNode(int drawX, int drawY, string skillName, GrowDirection growDirection = GrowDirection.None, bool unlocked = false)
+            {
+                this.drawX = drawX;
+                this.drawY = drawY;
+                this.skillName = skillName;
+                this.unlocked = unlocked;
+                this.growDirection = growDirection;
+            }
+
+            public SkillTreeNode(string skillName, GrowDirection growDirection = GrowDirection.None, bool unlocked = false)
+            {
+                this.drawX = 0;
+                this.drawY = 0;
+                this.skillName = skillName;
+                this.unlocked = unlocked;
+                this.growDirection = growDirection;
+            }
+
+            public SkillTreeNode addChild(string skillName)
+            {
+                return this.addChild(skillName, growDirection);
+            }
+
+            public SkillTreeNode addChild(string skillName, GrowDirection direction)
+            {
+                SkillTreeNode child = new SkillTreeNode(skillName, direction);
+                Children[direction].Add(child);
+                child.Parents.Add(this);
+                return child;
+            }
+
+            
+            public void unlock()
+            {
+                unlocked = true;
+            }
         }
+
 
         public void updateChildrenPositions()
         {
@@ -61,25 +82,20 @@ namespace EthoriaMod.Content
             }
         }
 
-        public void unlock()
+
+        public SkillTree(int dist = 10)
         {
-            unlocked = true;
-        }
-    }
-    public class SkillTree
-    {
+            this.dist = dist;
 
-
-
-        public SkillTreeNode root;
-        public SkillTree()
-        {
             root =  new SkillTreeNode(0, 0, "Start", SkillTreeNode.GrowDirection.None, true);
 
             root.addChild("Warrior", SkillTreeNode.GrowDirection.Left);
             root.addChild("Ranger", SkillTreeNode.GrowDirection.Right);
             root.addChild("Mage", SkillTreeNode.GrowDirection.Up);
             root.addChild("Summoner", SkillTreeNode.GrowDirection.Down);
+
+
+            this.updateChildrenPosition();
         }
         
 
